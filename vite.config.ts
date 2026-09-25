9183c30cd9518a5a88ec05a6f7a9d85bb5fe34f0
Leonid Belikov <lifeisgym@gmail.com>
2026-09-25 14:14:58 +0200

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    tsconfigPaths: true,
+  },
 })
